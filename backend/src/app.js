6b82227848b1c2d7003758d859083fc934e974a8
bhaskar0191 <tulasibhaskar91@gmail.com>
2026-06-19 10:32:37 +0530

    chage

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-app.get((req, res) => {
+app.get("/", (req, res) => {
     console.log("server is conncet")
 })
 
