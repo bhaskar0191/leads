c8db43d9e4dce6d4f8252ac755b5cd133101a045
bhaskar0191 <tulasibhaskar91@gmail.com>
2026-06-22 17:14:58 +0530

    add frontend Url

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ dotenv.config()
 const app = express();
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: "https://leads-frontend-mu.vercel.app/", 
     credentials:true
 }));
 
